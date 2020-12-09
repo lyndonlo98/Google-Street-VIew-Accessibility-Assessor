93940bac8691cf5dfc0d0599ede1ca6fad45c9e1
Roustom <47872392+Roustom@users.noreply.github.com>
2020-12-08 22:16:52 -0500

    changed components to material UI

--- src/App.js
+++ src/App.js
@@ -1,53 +1,89 @@
 import "./App.css";
 import "./map.js";
-import {
-  Tabs,
-  Tab,
-  TabContainer,
-  TabPane,
-  TabContent,
-  Row,
-  Col,
-  Nav,
-} from "react-bootstrap";
-import MapContainer from "./map.js";
-function App() {
-  return (
-    <div className="map">
-      <MapContainer />
-      <div className="left-overlay">
-        {/*SEARCH BAR*/}
-        <input
-          id="searchBar"
-          key="random1"
-          //value={keyword}
-          placeholder={"View accessbility in an area"}
-          // onChange={(e) => setKeyword(e.target.value)}
-        />
+import React from 'react';
 
-        {/*TABS*/}
-        <TabContainer id="left-tabs-example" defaultActiveKey="first">
-          <Row>
-            <Col sm={3}>
-              <Nav variant="pills" className="flex-column">
-                <Nav.Item>
-                  <Nav.Link eventKey="first">Plan a route</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">Get directions</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Col>
-            <Col sm={9}>
-              <TabContent>
-                <TabPane eventKey="first"></TabPane>
-                <TabPane eventKey="second"></TabPane>
-              </TabContent>
-            </Col>
-          </Row>
-        </TabContainer>
-      </div>
-    </div>
+import {Divider, makeStyles, Paper, IconButton, InputBase, Button, Box, BottomNavigation, BottomNavigationAction, Hidden  } from '@material-ui/core';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import EditIcon from '@material-ui/icons/Edit';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import DirectionsIcon from '@material-ui/icons/Directions';
+import MapContainer from "./map.js";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // visibility: "Hidden",
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 320,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+  bottomNavigation: {
+    marginTop: "5px",
+    width:325,
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  return (
+    <Box>
+      <MapContainer />
+
+      <Button 
+        variant="contained"
+        color="primary"
+        id="showOverlayButton"
+        endIcon={<ArrowForwardIosIcon/>}
+        >
+          View Accessibility
+      </Button>
+
+
+      <Box
+        id="leftOverlay">
+      <IconButton
+        id="hideOverlayButton">
+      <ArrowBackIosIcon
+        id="backArrowIcon"/>
+      </IconButton>
+
+      <Paper component="form" className={classes.root}>
+      <IconButton className={classes.iconButton} aria-label="menu">
+        <MenuIcon />
+      </IconButton>
+      <InputBase
+        className={classes.input}
+        placeholder="View Accessibility in an Area"
+        inputProps={{ 'aria-label': 'View accessibility in an area' }}
+      />
+      <IconButton type="submit" className={classes.iconButton} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+
+
+        <BottomNavigation
+          className={classes.bottomNavigation}
+          showLabels>
+          <BottomNavigationAction label="Plan a route" icon={<EditIcon />} />
+          <BottomNavigationAction label="Get directions" icon={<DirectionsIcon />} />
+        </BottomNavigation>
+      </Box>
+    </Box>
+    
   );
 }
 
