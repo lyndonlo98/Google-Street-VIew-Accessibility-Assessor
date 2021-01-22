5fc2e28cfb7b28a217633dccbcda9e172e4b2f1c
lyndonlo98 <lyndonlo98@gmail.com>
2021-01-21 22:23:27 -0500

    removed unused import

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+import { Auth } from 'aws-amplify';
+
 
 ReactDOM.render(
   <React.StrictMode>
