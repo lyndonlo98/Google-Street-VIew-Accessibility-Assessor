2371c2f330eaae59642589f65813217a22232537
Roustom <47872392+Roustom@users.noreply.github.com>
2021-02-03 11:54:09 -0500

    trying to dynamically set up routes

--- src/components/Routes/route.js
+++ src/components/Routes/route.js
@@ -1,33 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {DirectionsService, DirectionsRenderer } from '@react-google-maps/api';
+import { AddressContext } from '../../App';
 
-const directionsRequest = {
-    origin: '279 Laurier Ave W Suite 100, Ottawa, ON K1P 5J9',
-    destination: '267 Bank St, Ottawa, ON K2P 2L6',
-    provideRouteAlternatives: false,
-    travelMode: 'WALKING'
+const directionsRequest = (start,end)=> {
+
+    return {
+      origin: start,
+      destination: end,
+      provideRouteAlternatives: false,
+      travelMode: "WALKING",
+    };
+    
   }
 
-  const callbackFunction = (response) => {
-    console.log(response);
-    if(response!=null){
-      if (response.status === "OK") {
-        //route was returned successfully
-        setResponse(response);
-      } else {
-        console.log(response.status);
-      }
-    }
-}
+//   const callbackFunction = (response) => {
+//     console.log(response);
+//     if(response!=null){
+//       if (response.status === "OK") {
+//         //route was returned successfully
+//         setResponse(response);
+//       } else {
+//         console.log(response.status);
+//       }
+//     }
+// }
+/*
+function handleScriptLoad(response, setResponse){
 
+}
+*/
 const Route = () => {
     const [response, setResponse] = useState("");
-
+    const { addresses, addressDispatch } = useContext(AddressContext);
+    console.log(addresses.destination);
+    console.log(addresses.source);
+    /*
+    useEffect(() => {
+          handleScriptLoad(setResponse);
+      }, []);
+*/
     return(
     <div>
         <DirectionsService
-        options={directionsRequest}
-        callback={callbackFunction}/>
+        options={directionsRequest(addresses.source,addresses.destination)}
+        callback={(response) => {
+            console.log(response);
+            if(response!=null){
+              if (response.status === "OK") {
+                //route was returned successfully
+                setResponse(response);
+              } else {
+                console.log(response.status);
+              }
+            }
+        }}/>
 
         <DirectionsRenderer 
         options={{directions: response}}/>
