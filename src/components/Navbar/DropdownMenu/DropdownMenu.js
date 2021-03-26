aefbd92990f4c090d134dc9255b0400ca86344ba
Leo Paz <omarleopaz@hotmail.com>
2021-03-26 14:31:28 -0400

    Optimizing markers to not be so packed together🔥

--- src/components/Navbar/DropdownMenu/DropdownMenu.js
+++ src/components/Navbar/DropdownMenu/DropdownMenu.js
@@ -27,7 +27,7 @@ const DropdownMenu = () => {
     function DropdownItem(props) {
       return (
         <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
-          <span className="icon-button">{props.leftIcon}</span>
+          <span className="icon-button hover-button">{props.leftIcon}</span>
           {props.children}
           <span className="icon-right">{props.rightIcon}</span>
         </a>
